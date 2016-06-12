4cf8d02732d0409415e200b526d190b855e2b024
anupamsapre <sapre.an@husky.neu.edu>
2016-06-12 19:27:56 -0400

    All working

--- assignment/app.js
+++ assignment/app.js
@@ -1,16 +1,5 @@
 module.exports = function(app) {
-    var connectionString = 'mongodb://127.0.0.1:27017/test';
 
-    if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-        connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-            process.env.OPENSHIFT_APP_NAME;
-    }
-
-    var mongoose = require("mongoose");
-    mongoose.connect(connectionString);
     var models = require("./models/models.server")();
     require("./services/user.service.server.js")(app,models);
     require("./services/website.service.server.js")(app,models);
